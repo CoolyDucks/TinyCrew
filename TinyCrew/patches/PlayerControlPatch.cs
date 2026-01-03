5faa164a09d290291d189a31219eac776ded58b0
CoolyDucks <peguinboy2015@gmail.com>
2026-01-03 16:09:56 +0300

    Create PlayerControlPatch.cs

---
+++ TinyCrew/patches/PlayerControlPatch.cs
@@ -0,0 +1,14 @@
+using HarmonyLib;
+
+namespace TinyCrew.Patches;
+
+[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Start))]
+public static class PlayerControlPatch
+{
+    public static void Postfix(PlayerControl __instance) => __instance.UpdateSize();
+
+    public static void UpdateSize(this PlayerControl player)
+    {
+        player.cosmetics.SetScale(player.MyPhysics.Animations.DefaultPlayerScale, player.defaultCosmeticsScale);
+    }
+}
