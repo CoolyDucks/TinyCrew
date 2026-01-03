b80b916e5687a1f928cf29ca9d89d184a0718019
CoolyDucks <peguinboy2015@gmail.com>
2026-01-03 16:06:32 +0300

    Create TinyCrewPlugins.cs

---
+++ TinyCrew/TinyCrewPlugins.cs
@@ -0,0 +1,32 @@
+using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace TinyCrew;
+
+[BepInAutoPlugin("coolyducks.tinycrew")]
+[BepInProcess("Among Us.exe")]
+public partial class TinyCrewPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new("coolyducks.tinycrew");
+    public static ConfigEntry<float> ConfigScale { get; private set; }
+    public static float ScaleMod => ConfigScale.Value;
+
+    public override void Load()
+    {
+        ConfigScale = Config.Bind("Scale", "GlobalModifier", 2f, "");
+        Harmony.PatchAll();
+        ConfigScale.SettingChanged += (_, _) =>
+        {
+            if (HudManager.InstanceExists) HudStartPatch.UpdateSize();
+            foreach (var player in PlayerControl.AllPlayerControls) player.UpdateSize();
+        };
+        SceneManager.add_sceneLoaded((Action<Scene, LoadSceneMode>)((scene, _) =>
+        {
+            if (scene.name == "MainMenu") ModManager.Instance.ShowModStamp();
+        }));
+    }
+}
